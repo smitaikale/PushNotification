3766d6ec7e65b1e9557ef3c5c6f37dba4a640362
Smita Yashavant Ikale <P7111660@ntil.com>
2020-07-29 12:23:14 +0530

    initial

---
+++ sw.js
@@ -0,0 +1,20 @@
+self.addEventListener('push',function (e){
+
+    var options = {
+    body: 'you have one notification',
+    icon: 'images/example.png',
+    vibrate: [100,50,100],
+    data: {
+        dateOfArrival: Date.now(),
+        primaryKey:'2'
+    
+    },
+    actions:[
+        {action:'explore',title:'explore',icon:'images/checkmark.png'},
+        {action:'close',title:'Close',icon:'images/xmark.png'},
+    ]
+    };
+    e.waitUntil(
+        self.registration.showNotification('First Notification',options)
+    );
+    });
